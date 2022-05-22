249f24a3b7343abc183453863244f6daec92834e
Kirimito <ditoachanation@gmail.com>
2022-05-22 21:35:43 +0700

    menampilkan tanggal hari ini

--- index.php
+++ index.php
@@ -2,24 +2,16 @@
     <head>
       <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
       <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
-      <py-env>
-        - numpy
-        - matplotlib
-      </py-env>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     </head>
+
   <body>
-    <h1>Mari kita coba</h1>
-    <div id="plot"></div>
-    <py-script output="plot">
-      import matplotlib.pyplot as plt
-      import numpy as np
-      
-      x = np.random.randn(1000)
-      y = np.random.randn(1000)
-      
-      fig, ax = plt.subplots()
-      ax.scatter(x, y)
-      fig
+    <b><p>Today is <u><label id='today'></label></u></p></b>
+    <br>
+    <div id="pi" class="alert alert-primary"></div>
+    <py-script>
+import datetime as dt
+day = pyscript.write('today', dt.date.today().strftime('%A %B %d, %Y'))
     </py-script>
   </body>
 </html>
